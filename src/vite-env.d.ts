4994310fafb212447b2a0073406dd2e749699224
Daniel Henderson <djhindu17@gmail.com>
2026-02-26 19:34:42 -0600

    pwa-register installed

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,10 @@
 /// <reference types="vite/client" />
 
 declare const __APP_VERSION__: string
+
+declare module 'virtual:pwa-register' {
+  export function registerSW(options?: {
+    onNeedRefresh?: () => void
+    onOfflineReady?: () => void
+  }): (reloadPage?: boolean) => void
+}
